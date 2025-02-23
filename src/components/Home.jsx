8f2756fdb782aeb42cf4751c22246d5568e42e12
vaibhav <vaibhavsc107@gmail.com>
2025-02-23 18:37:03 +0530

    save all change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="content">
-      <h1>Auction</h1>
+      <h1>Auction</h1> <br />
       <section>
         <p>
           An auction is usually a process of buying and selling goods or services by offering them up for bids, taking bids, and then selling the item to the highest bidder or buying the item from the lowest bidder. Some exceptions to this definition exist and are described in the section about different types.
